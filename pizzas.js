ab47bd04e02c0924d2cc9353a112524de2e40362
MenorDev <andersonmkt97@gmail.com>
2025-02-15 14:03:17 -0300

    atualizando

--- pizzas.js
+++ pizzas.js
@@ -3,7 +3,7 @@ let pizzaJson = [
     id: 1,
     name: "Mussarela",
     img: "images/pizza.png",
-    price: 20.19,
+    price: 50.00,
     sizes: ["100g", "530g", "860g"],
     description:
       "Descrição da pizza em mais de uma linha muito legal bem interessante",
